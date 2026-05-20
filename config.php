34ef82e50b0dbfdb8d50eea10763cab15f490bd4
Zenith Kandel <kandelze123@gmail.com>
2026-05-20 13:46:44 +0545

    resr

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'pixelflap');
-define('DB_USER', 'pixelflap_user');
-define('DB_PASS', 'your_password_here');
-define('APP_NAME', 'PixelFlap');
-define('BASE_URL', 'http://localhost');
-define('ADMIN_DEFAULT_PASS', 'Admin1234!');
