530956b119abf77fd9d97ee7144eec2120f1878f
Zenith Kandel <kandelze123@gmail.com>
2026-05-20 14:38:00 +0545

    fix: update BASE_URL in config and enhance event log with additional debug and error entries

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ define('DB_NAME', 'pixelflap');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('APP_NAME', 'PixelFlap');
-define('BASE_URL', 'http://localhost/codes');
+define('BASE_URL', 'http://localhost/codes/pixelforge');
 define('ADMIN_DEFAULT_PASS', 'Admin1234!');
