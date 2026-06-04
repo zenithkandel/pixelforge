1cf586adb1849333644ab29b084e6ad21b4d6b8a
Zenith Kandel <kandelze123@gmail.com>
2026-06-04 21:34:37 +0545

    fix: add randFloat function to generate random float values between min and max

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -4,6 +4,11 @@ const Utils = {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     },
 
+    // Random float between min and max
+    randFloat(min, max) {
+        return Math.random() * (max - min) + min;
+    },
+
     // Random item from array
     randItem(arr) {
         return arr[Math.floor(Math.random() * arr.length)];
