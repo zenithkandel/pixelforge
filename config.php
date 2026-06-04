581575432312e168fe773914fef8b7b63d111282
Zenith Kandel <kandelze123@gmail.com>
2026-06-04 19:28:08 +0545

    reset

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'pixelflap');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('APP_NAME', 'PixelForge');
-define('BASE_URL', 'http://localhost/codes/pixelforge');
-define('ADMIN_DEFAULT_PASS', 'Admin1234!');
